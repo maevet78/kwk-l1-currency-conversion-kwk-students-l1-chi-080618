0986e0364d0fe4507a94ec5da741b145c18f1c7b
maevet78 <190439mt@saintviator.com>
2018-08-09 20:32:35 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,1 +1,31 @@
 # Write your cod here
+# Write two methods: one named `usd_to_eur` and one named `eur_to_usd`. The method
+# called `usd_to_eur` should take a dollar amount as an argument, and return the
+# equivalent value in Euros. The method `eur_to_usd` should do the reverse: it
+# should take an amount in Euros as an argument, and return the equivalent value
+# in US Dollars. Values of currencies fluctuate with the market, so you'll need to
+# do some research online to see their current market values.
+
+# To pass this challenge, it should be possible to convert dollars to euros, then
+# back to dollars and receive the same initial value. So, for example, if 1.20
+# dollars can be converted to 1 euro, converting 1 euro back to dollars should
+# return 1.20.
+
+def usd_to_eur(dollars)
+  us_dollars = ["$1", "$5", "$10", "$20", "$30", "$40", "$50", "$100"]
+    if euros == 0.86255 
+      puts us_dollars[0]
+    elsif euros = 4.31275
+      puts us_dollars[1]
+    elsif euros = 8.62550  
+      puts us_dollars[2]
+    elsif euros = 17.25100
+      puts us_dollars[3]
+    elsif euros = 43.12750
+      puts us_dollars[4]
+    elsif euros = 
+    
+    
+    
+    
+    
